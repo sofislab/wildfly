1e511eb7d844ec8707c80414d7146d8ce1f76fa3
Gabriel Haase <ghaase@sofis.com>
2019-05-17 13:23:27 -0300

    primera version

--- configs/run.sh
+++ configs/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 echo "··························································································"
@@ -6,7 +6,7 @@ echo "MARTIN VILCHE"
 echo "··························································································"
 
 if [ -z "$USERID" ]; then
-USERID=1001
+export USERID=1001
 echo "···································································································"
 echo "VARIABLE USERID NO ENCONTRADO USANDO POR DEFECTO " $USERID
 echo "···································································································"
@@ -26,7 +26,7 @@ echo "·····································
 echo "TIMEZONE SETEADO ENCONTRADO: " $TIMEZONE
 echo "···································································································"
 echo "SETEANDO TIMEZONE...."
-su-exec $USERID cat /usr/share/zoneinfo/$TIMEZONE > /etc/localtime
+cat /usr/share/zoneinfo/$TIMEZONE > /etc/localtime
 
 if [ $? -eq 0 ]; then
 echo "···································································································"
@@ -55,18 +55,18 @@ echo "........................."
 fi
 
 
-if [ ! -d /opt/wildfly10/.init ]; then
+if [ ! -d /opt/wildfly15/.init ]; then
 echo "........................."
 echo "JBOSS NO INICIALIZADO - DESCARGANDO.."
 echo "ESPERE POR FAVOR...."
-wget https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.tar.gz --progress=bar:force -O wildfly.tar.gz && \
-tar zxvf wildfly.tar.gz -C /opt/wildfly10 --strip-components 1  && chmod +x /opt/wildfly10/bin/* && \
-rm -rf wildfly.tar.gz /opt/wildfly10/docs /opt/wildfly10/welcome-content && \
-mkdir /opt/wildfly10/.init && \
-cp -rf /opt/configs/net /opt/wildfly10/modules/system/layers/base/ && \
-cp -rf /opt/configs/standalone.xml /opt/wildfly10/standalone/configuration/ && \
+wget https://download.jboss.org/wildfly/15.0.0.Final/wildfly-15.0.0.Final.tar.gz -O wildfly.tar.gz && \
+tar zxvf wildfly.tar.gz -C /opt/wildfly15 --strip-components 1  && chmod +x /opt/wildfly15/bin/* && \
+rm -rf wildfly.tar.gz /opt/wildfly15/docs /opt/wildfly15/welcome-content && \
+mkdir /opt/wildfly15/.init && \
+cp -rf /opt/configs/mysql /opt/wildfly15/modules/system/layers/base/com/ && \
+cp -rf /opt/configs/standalone.xml /opt/wildfly15/standalone/configuration/ && \
 echo "........................." && \
-echo "WILDFLY DESCARGADO CORRECTAMENTE EN /opt/wildfly10"
+echo "WILDFLY DESCARGADO CORRECTAMENTE EN /opt/wildfly15"
 echo "........................." 
 else
 echo "WILDFLY YA FUE INICIALIZADO - NO ES NECESARIO VOLVER A DESCARGAR"
@@ -75,12 +75,12 @@ fi
 
 echo "LIMPIANDO DIRECTORIOS"
 
-cd /opt/wildfly10/standalone/; rm -rf log/ tmp/ data/ configuration/standalone_xml_history deployments/*.deployed deployments/*.failed
+cd /opt/wildfly15/standalone/; rm -rf log/ tmp/ data/ configuration/standalone_xml_history deployments/*.deployed deployments/*.failed
 
 echo "........................."
 echo "FIX PERMISOS"
 chown $USERID:$USERID -R /opt/
 
-export JAVA_OPTS="$CUSTOM_JAVA_OPTS -DLOGLEVEL -DDATABASE-NAME -DDATABASE-HOST -DDATABASE-PORT -DDATABASE-USER -DDATABASE-PASSWORD -DJNDI-NAME -DPOOL-NAME -XX:+UseParallelGC -Dfile.encoding=UTF8 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -XX:+ExitOnOutOfMemoryError -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true"
+export JAVA_OPTS="$CUSTOM_JAVA_OPTS -DLOGLEVEL -DDATABASE-NAME -DDATABASE-HOST -DDATABASE-PORT -DDATABASE-USER -DDATABASE-PASSWORD -DJNDI-NAME -DPOOL-NAME -XX:+UseParallelGC -Dfile.encoding=UTF8 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true"
 echo "INICIANDO WILDFLY......"
 exec su-exec $USERID "$@"
